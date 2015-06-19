526a40c64d654db24f39c9e7fd577e802e013a8a
Ajinkya <ajinkya.parakh@ymail.com>
2015-06-19 23:17:22 +0530

    Added a simple frame

--- src/main/java/xyz/ajinkya/StktToCM/App.java
+++ src/main/java/xyz/ajinkya/StktToCM/App.java
@@ -1,13 +1,25 @@
 package xyz.ajinkya.StktToCM;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+  public static void main(String[] args) {
+    JFrame frame = new JFrame("StktToCM...");
+    frame.setSize(300, 300);
+    JLabel label = new JLabel();
+    Container content = frame.getContentPane();
+    content.add(label, BorderLayout.CENTER);
+    String message = "Lets take a look into CM";
+    label.setText(message);
+    frame.setVisible(true);
+  }
 }
